16dad655903dd5c3b2d7a2b53895f89bce0bd484
Mixail132 <millionbaxov@gmail.com>
2024-01-28 09:04:54 +0300

    Add a function gemini_ai() to views.py
    Now gemini-pro works as a function and you can call it from
    the telegram bot, or whatever.

--- geminitrial/views.py
+++ geminitrial/views.py
@@ -1,10 +1,17 @@
 import os
 from dotenv import load_dotenv
+
+
 load_dotenv()
 API_KEY = os.getenv("GOOGLE_API_KEY")
 os.environ['GOOGLE_API_KEY'] = API_KEY
+
+
 import google.generativeai as genai
 model = genai.GenerativeModel("gemini-pro")
-prompt = "Is that correct name? Agata Kristie"
-response = model.generate_content(contents=[prompt])
-print(response.text)
+
+
+def gemini_ai(prompt):
+    prompt = prompt
+    response = model.generate_content(contents=[prompt])
+    return response.text
