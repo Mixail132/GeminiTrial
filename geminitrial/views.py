6b790e7e267280fa236fe32963a4046bcc5fee6e
RuslanSlepuhin <101825832+RuslanSlepuhin@users.noreply.github.com>
2024-05-15 23:58:59 +0300

    exception code 429

--- geminitrial/views.py
+++ geminitrial/views.py
@@ -3,5 +3,8 @@ from models import model
 
 def gemini_ai(prompt):
     prompt = prompt
-    response = model.generate_content(contents=[prompt])
-    return response.text
+    try:
+        response = model.generate_content(contents=[prompt])
+        return response.text
+    except Exception as ex:
+        print(ex)
